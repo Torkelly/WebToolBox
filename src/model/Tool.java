d828e74877d3f3ed99a327f8a8d9faf5214b8879
Torkelly <Torkelly@users.noreply.github.com>
2019-10-08 20:13:20 -0500

    Update on Model/Persistence

--- src/model/Tool.java
+++ src/model/Tool.java
@@ -1,9 +1,56 @@
 package model;
 
-public class Tool {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="tools")
+
+public class Tool {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "TOOL_ID")
+	private int toolID;
+	@Column(name="TOOL_NAME")
+	private String toolName;
+	
 	public Tool() {
-		//can be changed based on database naming conventions
+		super();
+	}
+	
+	public Tool(int id, String toolName) {
+		super();
+		this.toolID = id;
+		this.toolName = toolName;
+	}
+	
+	public Tool(String toolName) {
+		super();
+		this.toolName = toolName;
 	}
 
+	public int getId() {
+		return toolID;
+	}
+
+	public void setId(int id) {
+		this.toolID = id;
+	}
+
+	public String getToolName() {
+		return toolName;
+	}
+
+	public void setToolName(String toolName) {
+		this.toolName = toolName;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Tool [id=" + toolID + ", toolName=" + toolName + "]";
+	}
 }
