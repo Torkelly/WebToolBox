3ce8001fd43e7f9e5b6a28bc1e10b2c4087e647b
Torkelly <Torkelly@users.noreply.github.com>
2019-10-14 08:52:20 -0500

    Updates on Controller
    CreateNewToolBoxServlet,
    LocalDatAttributeConverter,
    NavigateToolBoxesServlet

--- src/controller/CreateNewToolBoxServlet.java
+++ src/controller/CreateNewToolBoxServlet.java
@@ -1,9 +1,9 @@
-patbkage tbontroller;
+package controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.ToolBox;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
