275bebf0fa63e02450dcb38ee9cc8271bd809192
Torkelly <Torkelly@users.noreply.github.com>
2019-10-14 13:02:50 -0500

    Update on Servlets

---
+++ src/controller/NavigateToolsServlet.java
@@ -0,0 +1,76 @@
+  
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Tool;
+
+/**
+ * Servlet implementation class navigationServlet
+ */
+@WebServlet("/NavigateToolsServlet")
+public class NavigateToolsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public NavigateToolsServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ToolHelper th = new ToolHelper();
+		String act = request.getParameter("doThisToTool");
+
+		if (act == null) {
+			getServletContext().getRequestDispatcher("/ViewAllToolsServlet").forward(request, response);
+
+		} else if (act.equals("delete")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("toolId"));
+				Tool toolToDelete = th.searchForToolById(tempId);
+				th.deleteTool(toolToDelete);
+
+			} catch (NumberFormatException e) {
+				System.out.println("Forgot to click a button");
+			} finally {
+				getServletContext().getRequestDispatcher("/ViewAllToolsServlet").forward(request, response);
+			}
+
+		} else if (act.equals("edit")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("toolId"));
+				Tool toolToEdit = th.searchForToolById(tempId);
+				request.setAttribute("toolToEdit", toolToEdit);
+				getServletContext().getRequestDispatcher("/EditTool.jsp").forward(request, response);
+			} catch (NumberFormatException e) {
+				getServletContext().getRequestDispatcher("/ViewAllToolsServlet").forward(request, response);
+			} 
+
+		} else if (act.equals("add")) {
+			getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+		}
+
+	}
+
+}
--- src/controller/ViewAllToolBoxesServlet.java
+++ src/controller/ViewAllToolBoxesServlet.java
@@ -1,5 +1,51 @@
 package controller;
 
-public class ViewAllToolBoxesServlet {
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ToolBox;
+
+/**
+ * Servlet implementation class ViewAllToolBoxesServlet
+ */
+@WebServlet("/ViewAllToolBoxesServlet")
+public class ViewAllToolBoxesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewAllToolBoxesServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ToolBoxHelper tbh = new ToolBoxHelper();
+		List<ToolBox> ltb = tbh.getToolBoxes();
+		request.setAttribute("allToolBoxes", ltb);
+		
+		if(ltb.isEmpty()){
+				request.setAttribute("allToolBoxes", " ");
+		}
+
+		getServletContext().getRequestDispatcher("/ToolBoxByOwner.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
